95d2712a8ce9a866ae545fa7ce76791a257f0dce
Farhan2706 <farhan.khalid.2706@gmail.com>
2026-06-02 17:11:00 +0530

    patterns

--- Patterns/practice.c
+++ Patterns/practice.c
@@ -6,16 +6,19 @@ int main()
     printf("Enter a Number : ");
     scanf("%d",&n);
 
-    int a = 1;
-    int mid = (2*n+1)/2+1;
-    for (int i = 1; i <= 2*n+1; i++){ 
-        printf("%d",a);
-        if (i<mid){
-            a++;
+    int min = 0;
+    for (int i = 1; i <= 2*n-1; i++){
+        for (int j = 1; j <= 2*n-1; j++){
+            int a = i;
+            int b = j;
+            if(a>n) a = 2*n-i;
+            if (b>n) b = 2*n-j;
+            if (a>b) min = b;
+            else min = a;
+            printf("%d",n+1-min);
         }
-        else {
-            a--;
-        }
+        printf("\n");
     }
+
 return 0;
 }
